dc27ae94be2ef5852825e83c3491f49a95ce3558
GrapeJuice11111 <118233081+GrapeJuice11111@users.noreply.github.com>
2023-09-09 18:19:17 -0500

    Create A Call Back Constructor To Call Operation

--- javascript.js
+++ javascript.js
@@ -17,3 +17,18 @@ function multiply(a,b){
 let num1 = "";
 let num2 = "";
 let operator = "";
+
+function Operate(){
+    this.method = { 
+     "+" : add,
+     "-" : subtract,
+     "/" : divide,
+     "*" : multiply
+        }
+        
+  this.calculate = function calc(a,op,b){
+  return this.method[op](a,b); 
+  }
+}
+
+let calc = new Operate();
