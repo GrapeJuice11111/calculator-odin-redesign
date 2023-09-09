9fba112112adac6570050f2a361be83a02946028
GrapeJuice11111 <118233081+GrapeJuice11111@users.noreply.github.com>
2023-09-09 18:32:59 -0500

    Connect The JS File To The HTML

--- javascript.js
+++ javascript.js
@@ -32,3 +32,18 @@ function Operate(){
 }
 
 let calc = new Operate();
+
+// To display the number clicked on screen
+
+const display = document.querySelector(".display");
+let buttons = document.querySelectorAll("button");
+
+// Looping the button to see if a button is pressed with forEach
+
+buttons.forEach( btn => btn.addEventListener("click", function(e){
+
+    if(typeof(Number(this.classList.value)) === "number"){
+        num1 += this.classList.value;
+        display.textContent = num1;
+    }  
+}));
